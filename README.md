c2651858873af5c73e8e7df2dc6173336b59c06a
jacobrosenthal <jacob_rosenthal@mail.dfci.harvard.edu>
2019-07-31 11:17:34 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# pathml  
+
+**Data Science Group**  
+**Dana-Farber Cancer Institute**
+
+Utilities for working with pathology images, facilitating machine learning for digital pathology.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pathml",
+    version="0.0.1",
+    author="Jacob Rosenthal",
+    author_email="jacob_rosenthal@dfci.harvard.edu",
+    description="Utilities for digital pathology",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dana-Farber/pathml",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
