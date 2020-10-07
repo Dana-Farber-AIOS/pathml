541f141c8fe1fcbc4bd2b9ff4ab83736df911725
ryanccarelli <ryancarelli@gmail.com>
2020-10-07 14:30:19 -0400

    pass all MultiparametricSlide tests

--- tests/preprocessing_tests/test_wsi.py
+++ tests/preprocessing_tests/test_wsi.py
@@ -7,6 +7,7 @@ from pathml.preprocessing.wsi import HESlide, MultiparametricSlide
 
 try:
     import javabridge
+    import bioformats
     import bioformats.formatreader as biordr
     from bioformats.formatreader import ImageReader
 except ImportError:
@@ -29,6 +30,7 @@ def openslide_example():
     im_np_rgb = cv2.cvtColor(im_np, cv2.COLOR_RGBA2RGB)
     return im_np_rgb
 
+@pytest.fixture
 def smalltif_example():
     path = "tests/testdata/smalltif.tif"
     javabridge.start_vm(class_path=bioformats.JARS)
@@ -45,9 +47,9 @@ def test_HE_slide(openslide_example):
     slide_data2 = wsi.load_data(level = 0, location = (200, 200), size = (200, 200))
     assert np.array_equal(slide_data2.image, openslide_example[200:400, 200:400, :])
 
-def test_multiparametric_slide(confocal_zseries_multichannel_example):
-    wsi = MultiparametricSlide(path = "/tests/testdata/smalltif.tif"
+def test_multiparametric_slide(smalltif_example):
+    wsi = MultiparametricSlide(path = "tests/testdata/smalltif.tif")
     assert wsi.name == "smalltif"
-    assert wsi.path == "/tests/testdata/smalltif.tif"
+    assert wsi.path == "tests/testdata/smalltif.tif"
     slide_data = wsi.load_data()
     assert np.array_equal(slide_data.image, smalltif_example)
