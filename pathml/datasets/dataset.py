93d5bfa61bf06d1be688b74004247b95ed2a270a
ryanccarelli <ryancarelli@gmail.com>
2020-10-14 13:03:41 -0400

    init dataset class

---
+++ pathml/datasets/dataset.py
@@ -0,0 +1,26 @@
+class Dataset:
+    """
+    Class representing datasets composed of slides.
+    
+    """
+
+    def __init__(self, slides, labels=None):
+        pass
+        
+    def __getitem__(self):
+        pass
+
+    def make(self, pipeline):
+        # for slide in slides apply pipeline
+        # write slides to disk
+        # return reference to disk location?
+        # return dict
+        #   # slides
+        #   # tiles per slide
+        pass
+
+    def split():
+        pass
+
+
+
