4d5507dd5b56d51c85077c6a02054ba4c7e1295e
ryanccarelli <ryancarelli@gmail.com>
2020-11-25 12:07:07 -0500

    check mask shape, init with mask, getitem by mask name

--- pathml/preprocessing/masks.py
+++ pathml/preprocessing/masks.py
@@ -6,8 +6,13 @@ class Masks():
     Class wrapping OrderedDict of masks.
     Masks are type np.ndarray with elements type int8.
     '''
-    def __init__(self):
-        self._masks = OrderedDict()
+    def __init__(self, masks=None):
+        if masks:
+            if not isinstance(mask, np.ndarray):
+                raise ValueError(f"can not add {type(mask)}, mask must be of type np.ndarray")
+            self._masks = OrderedDict(masks)
+        else:
+            self._masks = OrderedDict()
 
     def __repr__(self):
         rep = f"Masks(keys={self._masks.keys()})"
@@ -16,18 +21,24 @@ class Masks():
     def __len__(self):
         return len(self._masks)
 
-    def __getitem__(self, idx):
-        if idx > len(self._masks)-1:
+    def __getitem__(self, item):
+        if isinstance(item, str):
+            return self._masks[item]
+        if item > len(self._masks)-1:
             raise KeyError(f"index out of range [0,{len(self._masks)-1}]") 
-        return list(self._masks.values())[idx]
+        return list(self._masks.values())[item]
 
     def add(self, key, mask):
         if not isinstance(mask, np.ndarray):
-            raise ValueError("can not add {type(mask)}, mask must be of type np.ndarray")
+            raise ValueError(f"can not add {type(mask)}, mask must be of type np.ndarray")
         if not isinstance(key, str):
-            raise ValueError("invalid type {type(key)}, key must be of type str")
+            raise ValueError(f"invalid type {type(key)}, key must be of type str")
         if key in self._masks:
             print(f"overwriting mask {key}")
+        if self._masks.keys():
+            requiredshape = self._masks[list(self._masks.keys())[0]].shape
+            if mask.shape != requiredshape:
+                raise ValueError(f"masks must be of shape {requiredshape} but provided mask is of shape {mask.shape}") 
         self._masks[key] = mask
 
     def remove(self, key):
