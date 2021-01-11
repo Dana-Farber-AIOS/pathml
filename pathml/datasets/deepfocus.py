c49ce738f584da1816b60cd517ced97441cb7647
ryanccarelli <ryancarelli@gmail.com>
2021-01-10 21:44:01 -0500

    deepfocus splits and debug

--- pathml/datasets/deepfocus.py
+++ pathml/datasets/deepfocus.py
@@ -1,17 +1,17 @@
 import os
 import ntpath
 import h5py
-from pathlib import path
+from pathlib import Path
 
 import torch.utils.data
 from torch.utils.data import Dataset, DataLoader
 
 from pathml.datasets.base import BaseDataModule, BaseDataset
-from pathml.ml.utils import download_url
+from pathml.datasets.utils import download_from_url
 
 class DeepFocusDataModule(BaseDataModule):
     """
-    Data from Cia lab DeepFocus experiments
+    Cia lab DeepFocus data.
     https://github.com/cialab/DeepFocus
     """
     def __init__(self, 
@@ -21,36 +21,39 @@ class DeepFocusDataModule(BaseDataModule):
             transforms = None,
             batch_size=8
     ):
-        if download is True:
-            self._download_deepfocus(data_dir) 
+        self.data_dir = Path(data_dir)
+        if download:
+            self._download_deepfocus(self.data_dir) 
         else:
-            assert data_dir.isdir(), f"download is False but data directory does not exist"
-
+            assert self.data_dir.is_dir(), f"download is False but data directory does not exist"
         self.shuffle = shuffle
-        self.transforms = trnasforms
+        self.transforms = transforms
         self.batch_size = batch_size
 
     def __len__(self):
         return len(self.datah5['X'])
 
-    def train_dataloader(self, split = 1):
+    @property
+    def train_dataloader(self):
         return data.DataLoader(
-                dataset = self._get_dataset(fold_ix = split),
-                batch_size = self.batch_size.
+                dataset = self._get_dataset(fold_ix = 1),
+                batch_size = self.batch_size,
                 shuffle = self.shuffle
         )
-        
-    def valid_dataloader(self, split = 2):
+    
+    @property
+    def valid_dataloader(self):
         return data.DataLoader(
-                dataset = self._get_dataset(fold_ix = split),
-                batch_size = self.batch_size.
+                dataset = self._get_dataset(fold_ix = 2),
+                batch_size = self.batch_size,
                 shuffle = self.shuffle
         )
 
-    def test_dataloader(self, split=3):
+    @property
+    def test_dataloader(self):
         return data.DataLoader(
-                dataset = self._get_dataset(fold_ix = split),
-                batch_size = self.batch_size.
+                dataset = self._get_dataset(fold_ix = 3),
+                batch_size = self.batch_size,
                 shuffle = self.shuffle
         )
     
@@ -66,12 +69,12 @@ class DeepFocusDataModule(BaseDataModule):
             print('File already downloaded')
             return
         # TODO: add md5 checksum
-        download_url('https://zenodo.org/record/1134848/files/outoffocus2017_patches5Classification.h5', root)
+        download_from_url('https://zenodo.org/record/1134848/files/outoffocus2017_patches5Classification.h5', root)
         # TODO: clean dataset
 
-    def _checkintegrity(self) -> bool:
+    def _check_integrity(self) -> bool:
         # TODO: check hash of file
-        return os.path.exists(path)
+        return os.path.exists(self.data_dir)
 
 class DeepFocusDataset(BaseDataset):
     def __init__(self,
@@ -79,13 +82,21 @@ class DeepFocusDataset(BaseDataset):
             fold_ix=None,
             transforms=None):
         self.datah5 = h5py.File(str(data_dir + 'outoffocus2017_patches5Classification.h5'), 'r')
-        # TODO: determine folds 
+        if fold_ix == None:
+            self.X = self.datah5['X']
+            self.Y = self.datah5['Y']
+        # train 80%
         if fold_ix == 1:
-            self.datah5 = self.datah5[]
+            self.X = self.datah5['X'][0:163199]
+            self.Y = self.datah5['Y'][0:163199]
+        # valid 10%
         if fold_ix == 2:
-            self.datah5 = self.datah5[]
+            self.X = self.datah5['X'][163200:183600]
+            self.Y = self.datah5['Y'][163200:183600]
+        # test 10%
         if fold_ix == 3:
-            self.datah5 = self.datah5[]
+            self.X = self.datah5['X'][183601:203999]
+            self.Y = self.datah5['Y'][183601:203999]
 
     def __len__(self):
         return len(self.datah5['X'])
@@ -94,4 +105,3 @@ class DeepFocusDataset(BaseDataset):
        img = self.datah5['X'][index]
        target = self.datah5['Y'][index]
        return img, target
-
