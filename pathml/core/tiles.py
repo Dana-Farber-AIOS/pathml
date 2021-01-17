df3e1ed3bedf80d4093e79f51d2c18e4c4aecebe
ryanccarelli <ryancarelli@gmail.com>
2021-01-17 17:01:07 -0500

    begin tempfile and h5, needs design decision

--- pathml/core/tiles.py
+++ pathml/core/tiles.py
@@ -9,18 +9,15 @@ from pathml.core.masks import Masks
 
 class Tile:
     """
-    Object for representing a tile extracted from an image. Holds the image for the tile, as well as the (i,j)
+    Object representing a tile extracted from an image. Holds the image for the tile, as well as the (i,j)
     coordinates of the top-left corner of the tile in the original image. The (i,j) coordinate system is based
     on labelling the top-leftmost pixel as (0, 0)
 
-    :param array: image for tile
-    :type array: np.ndarray
-    :param masks: masks for tile
-    :type masks: dict
-    :param i: vertical coordinate of top-left corner of tile, in original image
-    :type i: int
-    :param j: horizontal coordinate of top-left corner of tile, in original image
-    :type j: int
+    Args:
+        array (np.ndarray): tile image 
+        masks (dict, Masks): tile masks 
+        i (int): vertical coordinate of top-left corner of tile, in original image
+        j (int): horizontal coordinate of top-left corner of tile, in original image
     """
     def __init__(self, array, labels=None, masks=None, i=None, j=None):
         assert isinstance(array, np.ndarray), "Array must be a np.ndarray"
@@ -30,6 +27,7 @@ class Tile:
         assert isinstance(masks, (None, Masks, dict)), f"masks is of type {type(masks)} but must be of type pathml.core.Masks or dict"
         if isinstance(masks, Masks):
             self.masks = Masks
+        # populate Masks object by dict
         if isinstance(masks, dict): 
             for val in masks.values():
                 if val.shape != self.array.shape[:2]:
@@ -40,20 +38,23 @@ class Tile:
         assert isinstance(labels, (None, dict))
         self.labels = labels
         # initialize temp file
-        fd, path = tempfile.mkstemp()
-        # write tile, masks, labels to h5, then clear
-        # core?
+        # TODO: where do we first init the tempfile? 
+        # TODO: mkstemp or TemporaryFile
+        # fd = tempfile.TemporaryFile()
+        # fd, path = tempfile.mkstemp()
+        # write tile, masks, labels to h5 (which h5), then clear
+        # core file driver?
+        # libver (backwards compatibility) convention? 
         f = h5py.File(fd, 'w')
-        f.write
+        dset = f.create_dataset("tile", 
+        f['array'] = array
+        f['masks'] = masks
         self.h5 = path
 
     def __repr__(self):  # pragma: no cover
         return f"Tile(array shape {self.array.shape}, " \
                f"i={self.i if self.i is not None else 'None'}, " \
                f"j={self.j if self.j is not None else 'None'})"
-
-    def loadtile(self):
-        pass
 
     def save(self, out_dir, filename):
         """
