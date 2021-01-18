061bde88718671bcb4e2037e4cffb91e7bb1a586
ryanccarelli <ryancarelli@gmail.com>
2021-01-18 10:34:20 -0500

    init Tiles class, an OrderedDict composed of Tile

--- pathml/core/masks.py
+++ pathml/core/masks.py
@@ -1,7 +1,7 @@
 import numpy as np
 from collections import OrderedDict
 
-class Masks():
+class Masks:
     '''
     Object holding masks.  
 
--- pathml/core/tiles.py
+++ pathml/core/tiles.py
@@ -2,10 +2,94 @@ import numpy as np
 import os
 import cv2
 import shutil
+from typing import Union
 from pathlib import Path
 
 from pathml.core.masks import Masks
 
+class Tiles:
+    """
+    Object holding tiles.
+
+    Args:
+        tiles (Union[dict[tuple[int], `~pathml.core.tiles.Tile`], list]): tile objects  
+    """
+    def __init__(self,
+            tiles = None
+        ):
+        if tiles:
+            if not isinstance(tiles, (dict, list[Tile])):
+                raise ValueError(f"tiles must be passed as dicts of the form coordinate1:Tile1,... or lists of Tile objects containing i,j")
+            if isinstance(tiles, dict):
+                for val in tiles.values():
+                    if not isinstance(val, Tile):
+                        raise ValueError(f"dict vals must be Tile")
+                for key in tiles.values():
+                    if not isinstance(key, tuple[int]):
+                        raise ValueError(f"dict keys must be tuple[int]")
+                self._tiles = OrderedDict(tiles)
+            else:
+                tiledictionary = {}
+                for tile in tiles:
+                    tiledictionary[(tile.i, tile.j)] = tile
+                self._tiles = OrderedDict(tiledictionary)
+
+    def __repr__(self):
+        rep = f"Tiles(keys={self._tiles.keys()})"
+
+    def __len__(self):
+        return len(self._tiles)
+
+    def __getitem__(self, item):
+        if isinstance(item, tuple[int]):
+            return self._tiles[item]
+        if not isinstance(ite, int):
+            raise KeyERror(f"must getitem by coordinate(type tuple[int]) or index(type int)")
+        if item > len(self._tiles)-1:
+            raise KeyError(f"index out of range, valid indeces are ints in [0,{len(self._tiles)-1}]")
+        return list(self._tiles.values())[item]
+
+    def add(self, coordinate, tile):
+        """
+        Add tile indexed by coordinate to self._tiles.
+
+        Args:
+            coordinate(tuple[int]): location of tile on slide
+            tile(Tile): tile object
+        """
+        if not isinstance(tile, Tile):
+            raise ValueError(f"can not add {type(tile)}, tile must be of type pathml.core.tile.Tile")
+        if not isinstance(coordinate, tuple[int]):
+            raise ValueError(f"can not add type {type(key)}, key must be of type tuple[int]")
+        if coordinate in self._tiles:
+            print(f"overwriting tile at {coordinate}")
+        if self._tiles.keys():
+            requiredshape = self._tiles[list(self._tiles.keys())[0]].shape
+            if tile.array.shape != requiredshape:
+                raise ValueError(f"Tiles contains tiles of shape {requiredshape}, provided tile is of shape {tile.array.shape}. We enforce that all Tile in Tiles must have matching shapes.")
+        self._tiles[coordinate] = tile
+
+    def slice(self, coordinates):
+        """
+        Slice all tiles in self._tiles extending numpy array slicing
+
+        Args:
+            coordinates(tuple[int]): coordinates denoting slice i.e. 'selection' https://numpy.org/doc/stable/reference/arrays.indexing.html
+        """
+        tileslice = Tiles()
+        for key in self._tiles.keys():
+            val = self._tiles[key]
+            val = val[coordinates]
+            tileslice.add(key, val)
+        return tileslice
+
+    def remove(self, key):
+        """
+        Remove tile from self._tiles by key.
+        """
+        if key not in self._tiles:
+            raise KeyError('key is not in dict Tiles')
+        del self._tiles[key]
 
 class Tile:
     """
@@ -38,12 +122,11 @@ class Tile:
         assert isinstance(labels, (None, dict))
         self.labels = labels
         # initialize temp file
-        # TODO: where do we first init the tempfile? 
         # TODO: mkstemp or TemporaryFile
-        # fd = tempfile.TemporaryFile()
+        fd = tempfile.TemporaryFile()
         # fd, path = tempfile.mkstemp()
         # write tile, masks, labels to h5 (which h5), then clear
-        # core file driver?
+        # which .h5 file driver? 
         # libver (backwards compatibility) convention? 
         f = h5py.File(fd, 'w')
         dset = f.create_dataset("tile", 
