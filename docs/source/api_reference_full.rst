3642f037daa87c28eb8f0c5c97c1de48b7bd7cda
Jacob Rosenthal <jacob_rosenthal@dfci.harvard.edu>
2021-01-25 23:13:10 -0500

    tests for transforms. Also some reorganizing of test code to match source file structure

--- docs/source/api_reference_full.rst
+++ docs/source/api_reference_full.rst
@@ -4,27 +4,13 @@ Full API Reference
 Preprocessing
 -------------
 
-.. automodule:: pathml.preprocessing.wsi
-    :members:
-.. automodule:: pathml.preprocessing.multiparametricslide
-    :members:
-.. automodule:: pathml.preprocessing.slide_data
-    :members:
 .. automodule:: pathml.preprocessing.pipeline
     :members:
 .. automodule:: pathml.preprocessing.tiling
     :members:
-.. automodule:: pathml.preprocessing.stains
-    :members:
 .. automodule:: pathml.preprocessing.transforms
     :members:
-.. automodule:: pathml.preprocessing.transforms_HandE
-    :members:
-.. automodule:: pathml.preprocessing.utils
-    :members:
-.. automodule:: pathml.preprocessing.base
-    :members:
-
+    :undoc-members:
 
 Datasets
 --------
--- tests/preprocessing_tests/test_transforms.py
+++ tests/preprocessing_tests/test_transforms.py
@@ -2,69 +2,115 @@
 import cv2
 import numpy as np
 import pytest
+import openslide
+
+from pathml.core.tile import Tile
+from pathml.core.masks import Masks
 
 from pathml.preprocessing.transforms import (
     MedianBlur, GaussianBlur, BoxBlur, BinaryThreshold,
-    MorphOpen, MorphClose, ForegroundDetection
+    MorphOpen, MorphClose, ForegroundDetection, SuperpixelInterpolation,
+    StainNormalizationHE, NucleusDetectionHE, TissueDetectionHE
 )
+from pathml.utils import RGB_to_GREY
 
 
 @pytest.fixture
-def image():
-    return np.random.randint(low = 0, high = 256, size = (100, 100, 3), dtype = np.uint8)
+def tileHE():
+    """ Example of pathml.Tile object """
+    s = openslide.open_slide("tests/testdata/small_HE.svs")
+    im_image = s.read_region(level = 0, location = (900, 800), size = (500, 500))
+    im_np = np.asarray(im_image)
+    im_np_rgb = cv2.cvtColor(im_np, cv2.COLOR_RGBA2RGB)
 
-
-@pytest.fixture
-def mask():
-    """
-    mask that has a central square of ones with a circle of zeroes inside it
-    """
-    out = np.zeros((100, 100), dtype = np.uint8)
+    mask = np.zeros((im_np_rgb.shape[0], im_np_rgb.shape[1]), dtype = np.uint8)
     center = np.ones((50, 50))
     center_circle = cv2.getStructuringElement(shape = cv2.MORPH_ELLIPSE, ksize = (25, 25))
     center[12:37, 12:37] -= center_circle
-    out[25:75, 25:75] = center
-    return out
+    mask[25:75, 25:75] = center
 
-
-@pytest.mark.parametrize('transform', [
-    MedianBlur(), GaussianBlur(), BoxBlur(), MorphOpen(), MorphClose()
-])
-def test_image_transform(image, transform):
-    out = transform.apply(image)
-    assert out.shape == image.shape
-    assert out.dtype == image.dtype
+    m = Masks(masks = {"testmask" : mask})
+    tile = Tile(image = im_np_rgb, coords = (0, 0), masks = m, slidetype = "HE")
+    return tile
 
 
 @pytest.mark.parametrize('ksize', [3, 7, 21])
-def test_median_blur_large_kernel(image, ksize):
-    # for ksize > 5, dtype must be uint8
-    out = MedianBlur(kernel_size = ksize).apply(image)
-    assert out.shape == image.shape
-    assert out.dtype == image.dtype
+@pytest.mark.parametrize('transform', [MedianBlur, BoxBlur])
+def test_median_box_blur(tileHE, ksize, transform):
+    t = transform(kernel_size = ksize)
+    orig_im = tileHE.image
+    t.apply(tileHE)
+    assert np.array_equal(tileHE.image, t.F(orig_im))
 
 
-@pytest.mark.parametrize('transform', [
-    BinaryThreshold()
-])
-def test_segmentation_output_shape(image, transform):
-    """check Segmentation transforms to ensure that output is same (height, width) as input"""
-    out = transform.apply(image)
-    assert out.shape == image.shape[0:2]
-    assert out.dtype == np.uint8
+@pytest.mark.parametrize('ksize', [3, 7, 21])
+@pytest.mark.parametrize('sigma', [0.1, 3, 0.999])
+def test_gaussian_blur(tileHE, ksize, sigma):
+    t = GaussianBlur(kernel_size = ksize, sigma = sigma)
+    orig_im = tileHE.image
+    t.apply(tileHE)
+    assert np.array_equal(tileHE.image, t.F(orig_im))
 
 
-@pytest.mark.parametrize('transform', [
-    ForegroundDetection()
-])
-def test_mask_transform_output_shape(mask, transform):
-    """check MaskTransform to ensure that output is same shape as input"""
-    out = transform.apply(mask)
-    assert out.shape == mask.shape
-    assert out.dtype == np.uint8
+@pytest.mark.parametrize('thresh', [0, 0.5, 200])
+@pytest.mark.parametrize('otsu', [True, False])
+def test_binary_thresholding(tileHE, thresh, otsu):
+    t = BinaryThreshold(use_otsu = otsu, threshold = thresh, mask_name = "testing")
+    t.apply(tileHE)
+    assert np.array_equal(tileHE.masks["testing"], t.F(RGB_to_GREY(tileHE.image)))
 
 
-def test_foreground_detection_outer_contour_only(mask):
-    out = ForegroundDetection(outer_contours_only = True).apply(mask)
-    assert out.shape == mask.shape
-    assert out.dtype == np.uint8
+@pytest.mark.parametrize('kernel', [None, np.ones((4, 4), dtype = np.uint8)])
+@pytest.mark.parametrize('n_iter', [1, 3, 7, 21])
+@pytest.mark.parametrize('ksize', [3, 7, 21])
+@pytest.mark.parametrize('transform', [MorphOpen, MorphClose])
+def test_open_close(tileHE, transform, ksize, n_iter, kernel):
+    t = transform(kernel_size = ksize, n_iterations = n_iter, custom_kernel = kernel, mask_name = "testmask")
+    orig_mask = tileHE.masks["testmask"]
+    t.apply(tileHE)
+    assert np.array_equal(tileHE.masks["testmask"], t.F(orig_mask))
+
+
+@pytest.mark.parametrize('min_reg_size', [0, 10])
+@pytest.mark.parametrize('max_hole_size', [0, 10])
+@pytest.mark.parametrize('outer_contours_only', [True, False])
+def test_foreground_detection(tileHE, min_reg_size, max_hole_size, outer_contours_only):
+    t = ForegroundDetection(min_region_size = min_reg_size, max_hole_size = max_hole_size,
+                            outer_contours_only = outer_contours_only, mask_name = "testmask")
+    orig_mask = tileHE.masks["testmask"]
+    t.apply(tileHE)
+    assert np.array_equal(tileHE.masks["testmask"], t.F(orig_mask))
+
+
+@pytest.mark.parametrize('n_iter', [1, 30])
+@pytest.mark.parametrize('region_size', [10, 20])
+def test_superpix_interp(tileHE, region_size, n_iter):
+    t = SuperpixelInterpolation(region_size = region_size, n_iter = n_iter)
+    orig_im = tileHE.image
+    t.apply(tileHE)
+    assert np.array_equal(tileHE.image, t.F(orig_im))
+
+
+@pytest.mark.parametrize('target', ["normalize", "hematoxylin", "eosin"])
+@pytest.mark.parametrize('method', ["vahadane", "macenko"])
+def test_stain_normalization_he(tileHE, method, target):
+    t = StainNormalizationHE(target = target, stain_estimation_method = method)
+    orig_im = tileHE.image
+    t.apply(tileHE)
+    assert np.allclose(tileHE.image, t.F(orig_im))
+
+
+def test_nuc_detectionHE(tileHE):
+    t = NucleusDetectionHE(mask_name = "testing")
+    orig_im = tileHE.image
+    t.apply(tileHE)
+    assert np.array_equal(tileHE.masks["testing"], t.F(orig_im))
+
+
+@pytest.mark.parametrize('use_saturation', [True, False])
+@pytest.mark.parametrize('threshold', [None, 100])
+def test_tissue_detectionHE(tileHE):
+    t = TissueDetectionHE(mask_name = "testing")
+    orig_im = tileHE.image
+    t.apply(tileHE)
+    assert np.array_equal(tileHE.masks["testing"], t.F(orig_im))
