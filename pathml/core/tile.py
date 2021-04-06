d3e16127e1c81ee78b65ca60cf553e83361d6426
ryanccarelli <ryancarelli@gmail.com>
2021-04-06 15:32:51 -0400

    manage tile masks in memory instead of h5

--- pathml/core/tile.py
+++ pathml/core/tile.py
@@ -29,14 +29,18 @@ class Tile:
         assert name is None or isinstance(name, str), f"name is of type {type(name)} but must be of type str or None"
         self.image = image
         if isinstance(masks, pathml.core.masks.Masks):
-            self.masks = masks
+            # move masks to dict so that Tile is in memory (must pass to dask client) 
+            maskdict = OrderedDict()
+            for mask in masks.h5manager.h5.keys():
+                maskdict[mask] = masks[mask] 
+            self.masks = maskdict
         if isinstance(masks, dict):
             for val in masks.values():
                 if val.shape[:2] != self.image.shape[:2]:
                     raise ValueError(f"mask is of shape {val.shape} but must match tile shape {self.image.shape}")
-            self.masks = pathml.core.masks.Masks(masks)
+            self.masks = masks 
         elif masks is None:
-            self.masks = pathml.core.masks.Masks()
+            self.masks = OrderedDict() 
         self.name = name
         self.coords = coords
         self.slidetype = slidetype
