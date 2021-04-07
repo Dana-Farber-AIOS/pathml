29574dc52b503e8c9a6542613fd8939efc3ce0b9
ryanccarelli <ryancarelli@gmail.com>
2021-04-06 21:12:40 -0400

    test multiparametric

--- tests/core_tests/test_slide_classes.py
+++ tests/core_tests/test_slide_classes.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pathml.core.slide_classes import HESlide, RGBSlide
+from pathml.core.slide_classes import HESlide, RGBSlide, MultiparametricSlide 
 from pathml.core.slide_data import SlideData
 from pathml.core.slide_backends import OpenSlideBackend
 from pathml.core.tile import Tile
@@ -12,10 +12,21 @@ def he_slide():
     return wsi
 
 
+@pytest.fixture
+def multiparametric_slide():
+    wsi = MultiparametricSlide("tests/testdata/smalltif.svs") 
+    return wsi
+
+
 def test_he(he_slide):
     assert isinstance(he_slide, SlideData)
     assert isinstance(he_slide, RGBSlide)
     assert isinstance(he_slide.slide, OpenSlideBackend)
+
+
+def test_multiparametric(multiparametric_slide):
+    assert isinstance(multiparametric_slide, SlideData)
+    assert isinstance(multiparametric_slide.slide, BioFormatsBackend)
 
 
 @pytest.mark.parametrize("shape", [500, (500, 400)])
@@ -24,6 +35,14 @@ def test_he(he_slide):
 @pytest.mark.parametrize("level", [0])
 def test_generate_tiles_he(he_slide, shape, stride, pad, level):
     for tile in he_slide.generate_tiles(shape = shape, stride = stride, pad = pad, level = level):
+        assert isinstance(tile, Tile)
+
+
+@pytest.mark.parametrize("shape", [500, (500, 400)])
+@pytest.mark.parametrize("stride", [None, 1000])
+@pytest.mark.parametrize("pad", [True, False])
+def test_generate_tiles_multiparametric(multiparameteric_slide, shape, stride, pad):
+    for tile in multiparametric_slide.generate_tiles(shape = shape, stride = stride, pad = pad):
         assert isinstance(tile, Tile)
 
 
