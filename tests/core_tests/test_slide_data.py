6cade63036301498675cd76b5c5a1f388c10f881
Jacob Rosenthal <jacob_rosenthal@dfci.harvard.edu>
2021-04-09 00:31:34 -0400

    add test that fails when labels contains np array.

--- tests/core_tests/test_slide_data.py
+++ tests/core_tests/test_slide_data.py
@@ -1,7 +1,18 @@
-from pathml.core.slide_data import SlideData
+import numpy as np
+from pathlib import Path
 
-# test SlideData here
+from pathml.core.slide_data import SlideData
+from pathml.core.slide_backends import OpenSlideBackend
+
 
 def test_repr():
     s = SlideData()
     repr(s)
+
+
+def test_write_with_array_labels(tmp_path):
+    labs = {"testing": "testlabel", "test2": np.array([2, 3, 4])}
+    wsi = SlideData("tests/testdata/small_HE.svs", name = f"test_array_in_labels",
+                    labels = labs, slide_backend = OpenSlideBackend)
+    wsi.write(tmp_path / "test_array_in_labels.h5path")
+    assert Path(tmp_path / "test_array_in_labels.h5path").is_file()
