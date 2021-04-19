05faa14776afae0795ebf5f93252b5ed31a84fb4
ryanccarelli <ryancarelli@gmail.com>
2021-04-18 23:13:48 -0400

    add vectra workflow test

--- tests/integration_tests/test_preprocessing_workflow.py
+++ tests/integration_tests/test_preprocessing_workflow.py
@@ -1,8 +1,8 @@
 from dask.distributed import Client
 
-from pathml.core.slide_classes import HESlide
+from pathml.core.slide_classes import HESlide, VectraSlide
 from pathml.preprocessing.pipeline import Pipeline
-from pathml.preprocessing.transforms import BoxBlur, TissueDetectionHE
+from pathml.preprocessing.transforms import BoxBlur, TissueDetectionHE, DeconvolveMIF, SegmentMIF, QuantifyMIF
 
 
 def test_pipeline_1(tmp_path):
@@ -17,6 +17,23 @@ def test_pipeline_1(tmp_path):
     slide.run(pipeline, client = client, tile_size = 250)
 
     slide.tiles.write(out_dir = tmp_path, filename = "slidetiles.h5")
+
+    #shut down the client after running
+    client.close()
+
+def test_vectra_pipeline(tmp_path):
+    slide = VectraSlide("tests/testdata/vectra.tif")
+    pipeline = Pipeline([
+        DeconvolveMIF(),
+        SegmentMIF(model='mesmer', nuclear_channel=0, cytoplasm_channel=6, image_resolution=0.5),
+        QuantifyMIF()
+    ])
+
+    client = Client()
+
+    slide.run(pipeline, client = client, tile_size = 250)
+
+    slide.tiles.write(out_dir = tmp_path, filename = "vectratiles.h5")
 
     #shut down the client after running
     client.close()
