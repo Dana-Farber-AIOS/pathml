466a26b0e3629dc689ede4db206620ab9fe054dc
Jacob Rosenthal <jacob_rosenthal@dfci.harvard.edu>
2021-04-25 00:41:49 -0400

    add tests for dicom backend. Reorganize to test everything on all backends at once using fixtures

--- tests/core_tests/test_slide_backends.py
+++ tests/core_tests/test_slide_backends.py
@@ -1,73 +1,70 @@
 import pytest
 import numpy as np
-import openslide
 
 from pathml.core.slide_backends import OpenSlideBackend, DICOMBackend, BioFormatsBackend
 from pathml.core.tile import Tile
 
 
-@pytest.fixture
-def wsi_HE():
-    wsi = OpenSlideBackend("tests/testdata/small_HE.svs")
-    return wsi
+def openslide_backend():
+    return OpenSlideBackend("tests/testdata/small_HE.svs")
 
 
-@pytest.fixture
-def multiparametric_image():
-    image = BioFormatsBackend("tests/testdata/smalltif.tif") 
-    return image
+def bioformats_backend():
+    return BioFormatsBackend("tests/testdata/smalltif.tif")
 
 
+def dicom_backend():
+    return DICOMBackend("tests/testdata/small_dicom.dcm")
+
+
+## test each method for each backend
+
+@pytest.mark.parametrize("backend", [openslide_backend(), bioformats_backend(), dicom_backend()])
 @pytest.mark.parametrize("location", [(0, 0), (1000, 1000)])
-@pytest.mark.parametrize("size", [100, (500, 200)])
+@pytest.mark.parametrize("size", [500, (500, 500)])
 @pytest.mark.parametrize("level", [None, 0])
-def test_openslide_extract_tile(wsi_HE, location, size, level):
-    region = wsi_HE.extract_region(location = location, size = size, level = level)
-    assert isinstance(region, np.ndarray)
-    assert region.dtype == np.uint8
-
-    
-@pytest.mark.parametrize("location", [(0, 0), (1000, 1000)])
-@pytest.mark.parametrize("size", [100, (500, 200)])
-def test_bioformats_extract_tile(multiparametric_image, location, size):
-    region = multiparametric_image.extract_region(location = location, size = size)
+def test_openslide_extract_tile(backend, location, size, level):
+    region = backend.extract_region(location = location, size = size, level = level)
     assert isinstance(region, np.ndarray)
     assert region.dtype == np.uint8
 
 
+@pytest.mark.parametrize("backend,shape", [
+    (openslide_backend(), (2220, 2967)),
+    (bioformats_backend(), (640, 480)),
+    (dicom_backend(), (None, None))
+])
 @pytest.mark.parametrize("pad", [True, False])
-def test_openslide_tile_generator(wsi_HE, pad):
-    tiles = list(wsi_HE.generate_tiles(shape = 500, stride = 500, pad = pad, level = 0))
-    # small_HE.svs has dimensions (2220, 2967)
+@pytest.mark.parametrize("tile_shape", [500, (500, 500)])
+def test_openslide_tile_generator(backend, shape, tile_shape, pad):
+    tiles = list(backend.generate_tiles(shape = tile_shape, stride = 500, pad = pad, level = 0))
+    tile_shape = (tile_shape, tile_shape) if isinstance(tile_shape, int) else tile_shape
     if pad:
-        assert len(tiles) == 5 * 6
+        assert len(tiles) == np.prod([shape[i] // tile_shape[i] for i in range(len(shape))])
     else:
-        assert len(tiles) == 4 * 5
+        assert len(tiles) == np.prod([1 + shape[i] // tile_shape[i] for i in range(len(shape))])
     assert all([isinstance(tile, Tile) for tile in tiles])
 
 
-def test_openslide_get_image_shape(wsi_HE):
-    openslide_wsi = openslide.open_slide("tests/testdata/small_HE.svs")
-    # need to flip the dims because openslide uses (x, y) convention but we use (i, j)
-    assert wsi_HE.get_image_shape() == openslide_wsi.level_dimensions[0][::-1]
-    assert wsi_HE.get_image_shape(level = 0) == openslide_wsi.level_dimensions[0][::-1]
+@pytest.mark.parametrize("backend,shape", [
+    (openslide_backend(), (2220, 2967)),
+    (bioformats_backend(), (640, 480)),
+    (dicom_backend(), (None, None))
+])
+def test_openslide_get_image_shape(backend, shape):
+    assert backend.get_image_shape() == shape
 
 
-def test_openslide_get_thumbnail(wsi_HE):
-    thumbnail = wsi_HE.get_thumbnail(size = (500, 500))
+
+@pytest.mark.parametrize("backend", [openslide_backend(), bioformats_backend(), dicom_backend()])
+def test_get_thumbnail(backend):
+    print(dir(backend))
+    print(type(backend))
+    thumbnail = backend.get_thumbnail(size = (500, 500))
     assert isinstance(thumbnail, np.ndarray)
 
 
-def test_bioformats_get_thumbnail(multiparametric_image):
-    thumbnail = multiparametric_image.get_thumbnail(size = (500, 500))
-    assert isinstance(thumbnail, np.ndarray)
-
-
-def test_openslide_repr():
-    backend = OpenSlideBackend("tests/testdata/small_HE.svs")
-    repr(backend)
-
-
-def test_bioformats_repr():
-    backend = BioFormatsBackend("tests/testdata/smalltif.tif")
-    repr(backend)
+@pytest.mark.parametrize("backend", [openslide_backend(), bioformats_backend(), dicom_backend()])
+def test_repr(backend):
+    # make sure there are no errors during repr
+    print(backend)
