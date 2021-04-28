6ae51a11c9594a4100cec883fddb4382859e2d8d
Jacob Rosenthal <jacob_rosenthal@dfci.harvard.edu>
2021-04-28 14:16:32 -0400

    move utility function out of pannuke and into utils

--- pathml/datasets/utils.py
+++ pathml/datasets/utils.py
@@ -7,6 +7,8 @@ import os
 import shutil
 import urllib
 
+import numpy as np
+
 
 def parse_file_size(fs):
     """
@@ -46,3 +48,27 @@ def download_from_url(url, download_dir, name=None):
         # Download the file from `url` and save it locally under `file_name`:
         with urllib.request.urlopen(url) as response, open(path, 'wb') as out_file:
             shutil.copyfileobj(response, out_file)
+
+
+def pannuke_multiclass_mask_to_nucleus_mask(multiclass_mask):
+    """
+    Convert multiclass mask from PanNuke to a single channel nucleus mask.
+    Assumes each pixel is assigned to one and only one class. Sums across channels, except the last mask channel
+    which indicates background pixels in PanNuke.
+    Operates on a single mask.
+
+    Args:
+        multiclass_mask (torch.Tensor): Mask from PanNuke, in classification setting. (i.e. ``nucleus_type_labels=True``).
+            Tensor of shape (6, 256, 256).
+
+    Returns:
+        Tensor of shape (256, 256).
+    """
+    # verify shape of input
+    assert multiclass_mask.ndim == 3 and multiclass_mask.shape[0] == 6, \
+        f"Expecting a mask with dims (6, 256, 256). Got input of shape {multiclass_mask.shape}"
+    assert multiclass_mask.shape[1] == 256 and multiclass_mask.shape[2] == 256, \
+        f"Expecting a mask with dims (6, 256, 256). Got input of shape {multiclass_mask.shape}"
+    # ignore last channel
+    out = np.sum(multiclass_mask[:-1, :, :], axis = 0)
+    return out
