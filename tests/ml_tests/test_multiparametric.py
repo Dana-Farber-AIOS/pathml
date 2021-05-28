b44f5f040e77fb407b5c94b51df225b5fdbf238f
Jacob Rosenthal <jacob_rosenthal@dfci.harvard.edu>
2021-05-28 17:07:56 -0400

    fix some more tests

--- tests/ml_tests/test_multiparametric.py
+++ tests/ml_tests/test_multiparametric.py
@@ -10,16 +10,16 @@ from pathml.core import VectraSlide, Tiles
 from pathml.ml.multiparametric import spatialneighborhood
 
 def test_spatialneighborhood(tileVectra, anndata):
-    tiles = Tiles([tileVectra])
+    tiles = [tileVectra]
     adata = anndata
-    slidedata = VectraSlide(name='vectraslide', tiles=tiles)
+    slidedata = VectraSlide("tests/testdata/small_vectra.qptiff", name='vectraslide', tiles=tiles)
     slidedata.counts = adata
-    with pytest.raises(AssertionError):
+    with pytest.raises((AssertionError, ValueError)):
         spatialneighborhood(slidedata, phenotypekey='leiden')
     sc.pp.neighbors(slidedata.counts)
     sc.tl.leiden(slidedata.counts)
     sc.tl.pca(slidedata.counts)
     slidedata.counts.obsm['spatial'] = slidedata.counts.obsm['X_pca']
     print(slidedata.counts.obsm)
-    spatialneighborhood(slidedata, phenotypekey='leiden')
+    spatialneighborhood(slidedata, phenotypekey='leiden', n_neighbors = 2)
     assert slidedata.counts.obs.leiden_neighborhood
