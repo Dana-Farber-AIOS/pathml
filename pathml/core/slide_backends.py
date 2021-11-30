7c083eee7330320c82f23a3fe102972df5379ba6
Jacob Rosenthal <jacob_rosenthal@dfci.harvard.edu>
2021-11-30 10:30:22 -0500

    fix error in OpenSlideBackend extract_region

--- pathml/core/slide_backends.py
+++ pathml/core/slide_backends.py
@@ -97,14 +97,16 @@ class OpenSlideBackend(SlideBackend):
                 level < self.slide.level_count
             ), f"input level {level} invalid for a slide with {self.slide.level_count} levels"
 
+        # openslide read_region expects (x, y) coords, so need to switch order for compatibility with pathml (i, j)
+        i, j = location
+
         # openslide read_region() uses coords in the level 0 reference frame
         # if we are reading tiles from a higher level, need to convert to level 0 frame by multiplying by scale factor
         # see: https://github.com/Dana-Farber-AIOS/pathml/issues/240
-        coord_scale_factor = int(np.sqrt(self.slide.level_downsamples[level]))
-        location *= coord_scale_factor
+        coord_scale_factor = int(self.slide.level_downsamples[level])
+        i *= coord_scale_factor
+        j *= coord_scale_factor
 
-        # openslide read_region expects (x, y) coords, so need to switch order for compatibility with pathml (i, j)
-        i, j = location
         h, w = size
         region = self.slide.read_region(location=(j, i), level=level, size=(w, h))
         region_rgb = pil_to_rgb(region)
