bd3b1540b1ba269b302997794098dd8d0abc66c5
tddough98 <40068392+tddough98@users.noreply.github.com>
2022-10-20 17:41:34 -0400

    Change test to not expect KeyError

--- tests/core_tests/test_tiles.py
+++ tests/core_tests/test_tiles.py
@@ -148,16 +148,13 @@ def test_add_get(emptytiles, tileHE, incorrect_input, incorrect_input2):
 
 
 @pytest.mark.parametrize(
-    "incorrect_input", ["string", None, True, 5, [5, 4, 3], {"dict": "testing"}]
+    "incorrect_input", [None, True, 5, [5, 4, 3], {"dict": "testing"}]
 )
 def test_remove(emptytiles, tileHE, incorrect_input):
     tiles = emptytiles
     tiles.add(tileHE)
     tiles.remove((1, 3))
-    with pytest.raises(Exception):
-        triggerexception = tiles[(1, 3)]
-    with pytest.raises(KeyError):
-        tiles.remove((1, 3))
+    tiles.remove((1, 3))
     # incorrect input
     with pytest.raises(KeyError):
         tiles.remove(incorrect_input)
