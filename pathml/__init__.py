a57b7f66225b94a2414e585d9f5d20a2cd77cf89
jamesgwen <jgw787@gmail.com>
2023-08-11 10:07:14 -0700

    adding inference to init file

--- pathml/__init__.py
+++ pathml/__init__.py
@@ -6,6 +6,7 @@ License: GNU GPL 2.0
 from . import datasets as ds
 from . import ml
 from . import preprocessing as pp
+from .inference import * 
 from ._logging import PathMLLogger
 from ._version import __version__
 from .core import *  # noqa: F403
