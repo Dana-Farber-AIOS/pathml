b3d295a8ae2547551e0c3d3635e13c02f7d551f4
jamesgwen <jgw787@gmail.com>
2023-08-11 10:24:33 -0700

    fixing init files

--- pathml/__init__.py
+++ pathml/__init__.py
@@ -6,7 +6,7 @@ License: GNU GPL 2.0
 from . import datasets as ds
 from . import ml
 from . import preprocessing as pp
-import .inference 
+from . import inference
 from ._logging import PathMLLogger
 from ._version import __version__
 from .core import *  # noqa: F403
--- pathml/inference/__init__.py
+++ pathml/inference/__init__.py
@@ -5,9 +5,9 @@ License: GNU GPL 2.0
 
 from .inference import (
     remove_initializer_from_input,
-    check_onnx_clean, 
-    InferenceBase, 
-    Inference, 
-    HaloAIInference, 
+    check_onnx_clean,
+    InferenceBase
+    Inference,
+    HaloAIInference,
     RemoteTestHoverNet
 )
